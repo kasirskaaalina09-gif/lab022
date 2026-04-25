1225c612784d59b498f630e22d0ae280ae83314d
kasirskaaalina09-gif <kasirskaaalina09@gmail.com>
2026-04-25 21:43:15 +0000

    Update print.hpp

--- include/print.hpp
+++ include/print.hpp
@@ -1,6 +1,6 @@
 #include <fstream>
 #include <iostream>
 #include <string>
-
+//updated in main
 void print(const std::string& text, std::ofstream& out);
 void print(const std::string& text, std::ostream& out = std::cout);

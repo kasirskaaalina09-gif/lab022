e39d59d07af4d9e4caa498b9c4ecff85d17e0b92
kasirskaaalina09-gif <kasirskaaalina09@gmail.com>
2026-04-25 21:35:28 +0000

    applied clang-format style

--- sources/print.cpp
+++ sources/print.cpp
@@ -1,11 +1,13 @@
 #include <print.hpp>
 
-void print(const std::string& text, std::ostream& out)
+void
+print(const std::string& text, std::ostream& out)
 {
   out << text;
 }
 
-void print(const std::string& text, std::ofstream& out)
+void
+print(const std::string& text, std::ofstream& out)
 {
   out << text;
 }
